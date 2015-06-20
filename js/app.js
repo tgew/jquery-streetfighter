3e5e8eaf9b53bb5ce5fc8989d27902e1e37a254f
tgew <tim@goosemonkey.com>
2015-06-19 21:46:11 -0400

    added mouseup mousedown animations and sound

--- js/app.js
+++ js/app.js
@@ -8,12 +8,17 @@ $(document).ready(function () {
         $('.ryu-still').show();
     })
     .mousedown(function () {
-        console.log('mouse down');
+        playHadouken();
         $('.ryu-ready').hide();
         $('.ryu-throwing').show();
-        $('.hadouken').show();
-        // play h sound
-        // animate h
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function () {
+                $(this).hide();
+                $(this).css('left', '520px');
+            }
+        );
     })
     .mouseup(function () {
         console.log('mouse up');
@@ -23,3 +28,9 @@ $(document).ready(function () {
     });
     
 });
+
+function playHadouken () {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
